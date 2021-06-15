d3cfbfe255a363bcd696afa7d9d4308c224f8b44
ssquare143 <62537472+ssquare143@users.noreply.github.com>
2021-06-15 07:36:57 +0530

    retry

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express()
 const server = require('http').Server(app)
 const io = require('socket.io')(server)
 const { v4: uuidV4 } = require('uuid')
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
