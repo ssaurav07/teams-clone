809701e5f0431d8c128ac4f4746c184a1a73cb2c
ssquare143 <62537472+ssquare143@users.noreply.github.com>
2021-06-20 08:19:33 +0530

    screen share added

--- public/script.js
+++ public/script.js
@@ -4,11 +4,14 @@ const myPeer = new Peer({host:'peerjs-server.herokuapp.com', secure:true, port:4
 const videoGrid =  document.getElementById('video-grid');
 const muteAudio = document.getElementById('mute-mic');
 const muteVideo = document.getElementById('mute-vid');
+const shareScreen = document.getElementById('share-screen');
 
 muteAudio.onclick = function() { muteMic() }
 muteVideo.onclick = function() { muteCam() }
+shareScreen.onclick = function() { screenShare() }
 
 let myStream;
+var currPeer;
 
 const myVideo = document.createElement('video')
 myVideo.muted = true
@@ -24,6 +27,7 @@ navigator.mediaDevices.getUserMedia({
     call.answer(stream)
     const video = document.createElement('video')
     call.on('stream', userVideoStream => {
+      currPeer = call.peerConnection
       addVideoStream(video, userVideoStream)
     })
   })
@@ -52,6 +56,7 @@ function connectToNewUser(userId, stream) {
   const video = document.createElement('video')
   call.on('stream', userVideoStream => {
     addVideoStream(video, userVideoStream)
+    currPeer = call.peerConnection
   })
   call.on('close', () => {
     video.remove();
@@ -76,4 +81,24 @@ function muteMic() {
 
 function muteCam() {
   myStream.getVideoTracks().forEach(track => track.enabled = !track.enabled);
+}
+
+function screenShare (){
+navigator.mediaDevices.getDisplayMedia({
+  video: {
+    cursor : "always"
+  },
+  audio: {
+    echoCancellation : true,
+    noiseSuppression : true
+  }
+}).then((stream)=>{
+  let videoTrack = stream.getVideoTracks()[0];
+  let sender = currPeer.getSenders().find( function(s){
+    return s.track.kind == videoTrack.kind
+  })
+  sender.replaceTrack(videoTrack);
+}).catch((err)=>{
+  console.log("unable to display media" + err);
+})
 }
