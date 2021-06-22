3d01f5aae245fb908b2834c4a3ed86a0c7fdaa1e
ssquare143 <62537472+ssquare143@users.noreply.github.com>
2021-06-22 17:01:23 +0530

    node - chat

--- server.js
+++ server.js
@@ -17,13 +17,48 @@ app.use(express.static('public'))
 
 app.get('/', (req, res) => {
   res.redirect(`/${uuidV4()}`)
+  // res.redirect('/chat/app');
 })
 
 app.get('/:room', (req, res) => {
   res.render('room', { roomId: req.params.room })
 })
 
+app.get('/chat/app', (req, res) => {
+  res.render('chat')
+})
+
+
+const users = {}
+
+// io.on('connection', socket => {
+//   socket.on('new-user', name => {
+//     users[socket.id] = name
+//     socket.broadcast.emit('user-connected', name)
+//   })
+//   socket.on('send-chat-message', message => {
+//     socket.broadcast.emit('chat-message', { message: message, name: users[socket.id] })
+//   })
+//   socket.on('disconnect', () => {
+//     socket.broadcast.emit('user-disconnected', users[socket.id])
+//     delete users[socket.id]
+//   })
+// })
+
 io.on('connection', socket => {
+
+  socket.on('new-user', name => {
+    users[socket.id] = name
+    socket.broadcast.emit('user-connected', name)
+  })
+  socket.on('send-chat-message', message => {
+    socket.broadcast.emit('chat-message', { message: message, name: users[socket.id] })
+  })
+  socket.on('disconnect', () => {
+    socket.broadcast.emit('user-disconnected', users[socket.id])
+    delete users[socket.id]
+  })
+
   socket.on('join-room', (roomId, userId) => {
     socket.join(roomId)
     socket.broadcast.to(roomId).emit('user-connected', userId)
