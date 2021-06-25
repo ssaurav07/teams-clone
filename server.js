e6f732a2c89244b47c50eff7957a02cdfe963ce1
ssquare143 <62537472+ssquare143@users.noreply.github.com>
2021-06-25 11:22:59 +0530

    homepage finally fixed

--- server.js
+++ server.js
@@ -16,8 +16,7 @@ app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-  res.render('explore')
-  // res.redirect(`/${uuidV4()}`)
+  res.render('home')
 })
 
 app.get('/register', (req, res) => {
